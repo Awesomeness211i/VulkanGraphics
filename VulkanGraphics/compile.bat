44bb9e2106e0a24546c7d1efdf4ecbb90936aa8d
Christopher Iman <chrisiman53@gmail.com>
2022-03-12 18:37:21 -0700

    deleted unused files

--- VulkanGraphics/compile.bat
+++
@@ -1,3 +0,0 @@
-C:\VulkanSDK\1.2.176.1\Bin32\glslc.exe assets\shaders\shader.vert -o assets\shaders\shader.vert.spv
-C:\VulkanSDK\1.2.176.1\Bin32\glslc.exe assets\shaders\shader.frag -o assets\shaders\shader.frag.spv
-pause
--- VulkanGraphics/src/main.ixx
+++
@@ -1,9 +0,0 @@
-export module main;
-import Application;
-
-export int main() {
-	Florencia::Application* app = new Florencia::Application();
-	app->Run();
-	delete app;
-	return 0;
-}
